1915b5b33b2c04beed8e0815b112474d256bbb30
thelazybusycoder <thelazybusycoder@gmail.com>
2023-10-07 14:57:42 +0530

    update

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<String> al = new ArrayList<>(List.of(new String[]{"one" , "two"}));
+    System.out.println(al);
+  }
+}
